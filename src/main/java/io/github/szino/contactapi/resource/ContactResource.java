d948cc83f23031db3cfccee49b531e9130ed18b8
SziNo <szino1848@gmail.com>
2025-04-02 21:23:24 +0200

    refactor deleteContact in ContactService and implement a method for it in ContactResource

--- src/main/java/io/github/szino/contactapi/resource/ContactResource.java
+++ src/main/java/io/github/szino/contactapi/resource/ContactResource.java
@@ -49,4 +49,10 @@ public class ContactResource {
     public byte[] getPhoto(@PathVariable("filename") String filename) throws IOException {
         return Files.readAllBytes(Paths.get(PHOTO_DIRECTORY + filename));
     }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteContact(@PathVariable(value = "id") String id) {
+        contactService.deleteContact(id);
+        return ResponseEntity.noContent().build();
+    }
 }
